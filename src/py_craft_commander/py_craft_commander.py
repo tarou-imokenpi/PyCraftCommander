1960b43bc5fc0745e3074fc72bbd02e1ff32fe62
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-03 08:24:59 +0900

    feat： get_player_listメソッドにエラー処理を追加

--- src/py_craft_commander/py_craft_commander.py
+++ src/py_craft_commander/py_craft_commander.py
@@ -5,6 +5,9 @@ class PyCraftCommander(RCON):
     def __init__(self, host, port, password):
         super().__init__(host, port, password)
 
-    def get_player_list(self):
+    def get_player_list(self) -> list[str]:
         """マインクラフトサーバーのプレイヤーリストを取得します。"""
-        return self.send_command("list")
+        response, status = self.send_command("list")
+        if not status:
+            return []
+        return response.split(": ")[1].split(", ")
