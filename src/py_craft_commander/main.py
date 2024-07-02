e729baabbc7c5fec929129fee317d35261f88493
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-03 08:11:00 +0900

    PyCraftCommanderを利用するように変更

--- src/py_craft_commander/main.py
+++ src/py_craft_commander/main.py
@@ -1,9 +1,16 @@
-from rcon import RCON
+from py_craft_commander import PyCraftCommander
 
 host = "localhost"
 port = 25575
 password = "admin"
 
-with RCON(host, port, password) as r:
-    if r.auth():
-        print(r.command("say Hello, World!"))
+with PyCraftCommander(host, port, password) as server:
+    server.auth()
+
+    body, status = server.send_command("say Hello, World!")
+    if not status:
+        print("正しいレスポンスIDが返ってきませんでした。")
+        exit(1)
+
+    print("リクエスト成功！")
+    print(f"応答:{body}")
