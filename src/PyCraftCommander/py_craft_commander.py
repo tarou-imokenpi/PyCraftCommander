68cefb40fcd4675ba7c3abedab116502e5c367e3
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-03 19:57:06 +0900

    feat: プレイヤー情報を取得するためのget_player_infoメソッドを追加

--- src/PyCraftCommander/py_craft_commander.py
+++ src/PyCraftCommander/py_craft_commander.py
@@ -1,4 +1,5 @@
 from PyCraftCommander.rcon import RCON
+from PyCraftCommander.types.player import Player, Pos
 
 
 class PyCraftCommander(RCON):
@@ -34,3 +35,34 @@ class PyCraftCommander(RCON):
 
         response, status = self.send_command(f"tp {from_} {to}")
         return response
+
+    def get_player_info(self, player: str) -> Player:
+        """プレイヤーの情報を取得します。
+
+        Args:
+        -----
+            player (str): プレイヤー名
+
+        Returns:
+        -------
+            Player: プレイヤーオブジェクト
+        """
+        response, status = self.send_command(f"data get entity {player} Pos")
+        if not status:
+            return None
+        pos = response.split(": ")[-1]
+        pos = pos.translate(str.maketrans("", "", "[]")).split(", ")
+
+        x, y, z = float(pos[0][:-1]), float(pos[1][:-1]), float(pos[2][:-1])
+
+        response, status = self.send_command(f"data get entity {player} Dimension")
+        if not status:
+            return None
+        dimension = response.split(" ")[-1]
+
+        response, status = self.send_command(f"data get entity {player} playerGameType")
+        if not status:
+            return None
+        gamemode = response.split(" ")[-1]
+
+        return Player(player, Pos(x, y, z), dimension, gamemode)
