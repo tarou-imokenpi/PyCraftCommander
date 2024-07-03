77c30e288b2cab2505d0930e8d2f687a02a047c5
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-03 18:22:11 +0900

    Player型を追加

--- src/PyCraftCommander/main.py
+++ src/PyCraftCommander/main.py
@@ -7,7 +7,9 @@ password = "admin"
 with PyCraftCommander(host, port, password) as server:
     server.auth()
 
-    player_list, _ = server.get_player_list()
+    player_list = server.get_player_list()
     # if status:
     #     print("リクエスト成功")
     print(f"プレイヤーリスト:{player_list}")
+
+    print(server.send_command(f"data get entity {player_list[0]} Pos"))
---
+++ src/PyCraftCommander/types/player.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from enum import IntEnum
+
+
+@dataclass
+class Pos:
+    x: float
+    y: float
+    z: float
+
+
+class GameMode(IntEnum):
+    SURVIVAL = 0
+    CREATIVE = 1
+    ADVENTURE = 2
+    SPECTATOR = 3
+
+
+@dataclass
+class Player:
+    """プレイヤー情報を格納するデータクラス
+
+    Attributes:
+    ----------
+    name: プレイヤー名
+    Pos: プレイヤーの座標(x,y,z)
+    dimension: プレイヤーのディメンション
+    gamemode: ゲームモード
+    """
+
+    name: str
+    pos: Pos
+    dimension: str
+    gamemode: GameMode | str
