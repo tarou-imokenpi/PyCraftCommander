554ca06c027eaee50a1d5d7646590cdfca5f8a87
Shotaro Fukuoka <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-03 14:13:58 +0900

    tpメソッドを追加しバージョンを0.0.5にアップデート

--- src/PyCraftCommander/main.py
+++ src/PyCraftCommander/main.py
@@ -7,6 +7,7 @@ password = "admin"
 with PyCraftCommander(host, port, password) as server:
     server.auth()
 
-    player_list = server.get_player_list()
-
+    player_list, status = server.get_player_list()
+    if status:
+        print("リクエスト成功")
     print(f"プレイヤーリスト:{player_list}")
--- src/PyCraftCommander/py_craft_commander.py
+++ src/PyCraftCommander/py_craft_commander.py
@@ -1,13 +1,24 @@
 from PyCraftCommander.rcon import RCON
+from typing import Literal
 
 
 class PyCraftCommander(RCON):
+    TARGET = Literal["@a", "@r", "@e", "@p"]
+
     def __init__(self, host, port, password):
         super().__init__(host, port, password)
 
-    def get_player_list(self) -> list[str]:
+    def get_player_list(self) -> tuple[list[str], bool]:
         """マインクラフトサーバーのプレイヤーリストを取得します。"""
         response, status = self.send_command("list")
         if not status:
-            return []
-        return response.split(": ")[1].split(", ")
+            return [], False
+        return response.split(": ")[1].split(", "), True
+
+    def tp(self, from_: TARGET | str, to: TARGET | str) -> tuple[str, bool]:
+        """プレイヤーをテレポートします。"""
+        if from_ == to and from_ not in self.TARGET:
+            return "同じプレイヤーにはテレポートできません。", False
+
+        response, status = self.send_command(f"tp {from_} {to}")
+        return response, status
