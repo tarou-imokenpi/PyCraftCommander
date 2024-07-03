301fc1cf0399ae40df6bf455ae94d43387b7ffc2
tarou-imokenpi <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-03 17:25:44 +0900

    doc: get_player_listとtpにdocを追加

--- src/PyCraftCommander/py_craft_commander.py
+++ src/PyCraftCommander/py_craft_commander.py
@@ -3,21 +3,34 @@ from typing import Literal
 
 
 class PyCraftCommander(RCON):
-    TARGET = Literal["@a", "@r", "@e", "@p"]
-
     def __init__(self, host, port, password):
         super().__init__(host, port, password)
 
     def get_player_list(self) -> list[str]:
-        """マインクラフトサーバーのプレイヤーリストを取得します。"""
+        """マインクラフトサーバーのプレイヤーリストを取得します。
+
+        Returns:
+        -------
+            list[str]: プレイヤーリスト
+        """
         response, status = self.send_command("list")
         if not status:
             return []
         return response.split(": ")[1].split(", ")
 
-    def tp(self, from_: TARGET | str, to: TARGET | str) -> str:
-        """プレイヤーをテレポートします。"""
-        if from_ == to and from_ not in self.TARGET:
+    def tp(self, from_: str, to: str) -> str:
+        """プレイヤーをテレポートします。
+
+        Args:
+        -----
+            from_ (str): 移動させるプレイヤー
+            to (str): 移動先のプレイヤー
+
+        return:
+        ------
+            str: レスポンスメッセージ
+        """
+        if from_ == to and (from_ not in "@" or to not in "@"):
             return "同じプレイヤーにはテレポートできません。", False
 
         response, status = self.send_command(f"tp {from_} {to}")
