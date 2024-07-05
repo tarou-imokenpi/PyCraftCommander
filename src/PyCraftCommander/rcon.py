450f567eb6a035d805e10169164e4a7007855494
Shotaro Fukuoka <91922753+tarou-imokenpi@users.noreply.github.com>
2024-07-05 14:10:45 +0900

    rconの属性のプライベート化

--- src/PyCraftCommander/rcon.py
+++ src/PyCraftCommander/rcon.py
@@ -8,18 +8,18 @@ class RCON:
     """RCONプロトコルを用いてMinecraftサーバーにコマンドを送信するためのクラス"""
 
     # 4バイトの符号付き整数フォーマット
-    i32 = struct.Struct("<i")
+    __i32 = struct.Struct("<i")
     # responseパケットの構造
-    res_struct = struct.Struct("<iii")
+    __res_struct = struct.Struct("<iii")
 
     def __init__(self, host, port, password):
-        self.host = host
-        self.port = port
-        self.password = password
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__host = host
+        self.__port = port
+        self.__password = password
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            self.socket.connect((self.host, int(self.port)))
-            self.socket.settimeout(5.0)
+            self.__socket.connect((self.__host, int(self.__port)))
+            self.__socket.settimeout(5.0)
         except ConnectionRefusedError:
             raise ConnectionRefusedError(
                 "サーバーに接続できませんでした。\nサーバが起動していてホスト名とポート番号が正しいか確認してください。"
@@ -29,17 +29,17 @@ class RCON:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.socket.close()
+        self.__socket.close()
 
     def __del__(self):
-        self.socket.close()
+        self.__socket.close()
 
     def auth(self):
         """RCON認証を行います。"""
-        response_id = self.send_packet(PacketType.SERVERDATA_AUTH, self.password)
-        return self.auth_response(response_id)
+        response_id = self.send_packet(PacketType.SERVERDATA_AUTH, self.__password)
+        return self.__auth_response(response_id)
 
-    def auth_response(self, request_id: int):
+    def __auth_response(self, request_id: int):
         """RCON認証のレスポンスを受信します。"""
         packet: Packet = self.receive_packet()
         if (
@@ -77,21 +77,21 @@ class RCON:
         # 4    | 4  | 4    | size + 1    | 1
 
         body: bytes = body.encode("utf-8")
-        type: bytes = self.i32.pack(int(type))
+        type: bytes = self.__i32.pack(int(type))
 
-        packet_size: bytes = self.i32.pack(len(body) + 10)
+        packet_size: bytes = self.__i32.pack(len(body) + 10)
 
         # ランダムなパケットIDを生成(4バイトの符号付き整数) 2147483647
         ramdom_id = random.randint(0, 2147483647)
-        request_id: bytes = self.i32.pack(ramdom_id)
+        request_id: bytes = self.__i32.pack(ramdom_id)
 
         packet: bytes = packet_size + request_id + type + body + b"\x00\x00"
-        self.socket.sendall(packet)
+        self.__socket.sendall(packet)
         return ramdom_id
 
     def receive_packet(self) -> Packet:
         """パケットを受信し、Packetを返します"""
-        respone = self.socket.recv(4096)
-        size, request_id, type = self.res_struct.unpack(respone[:12])
+        respone = self.__socket.recv(4096)
+        size, request_id, type = self.__res_struct.unpack(respone[:12])
         body = respone[12:-2]
         return Packet(size, request_id, type, body)
